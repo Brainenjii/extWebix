2f61b89d63c1a78dff9bd7fa62443bbeea2158d1
Theo <brainenjii@gmail.com>
2015-04-28 12:31:07 +0300

    move books.json from example code

--- examples/grids/app.js
+++ examples/grids/app.js
@@ -1,47 +1,3 @@
-var data = [{
-  "id": "978-0641723445",
-  "cat": ["book", "hardcover"],
-  "name": "The Lightning Thief",
-  "author": "Rick Riordan",
-  "series_t": "Percy Jackson and the Olympians",
-  "sequence_i": 1,
-  "genre_s": "fantasy",
-  "inStock": true,
-  "price": 12.50,
-  "pages_i": 384
-}, {
-  "id": "978-1423103349",
-  "cat": "book",
-  "name": "The Sea of Monsters",
-  "author": "Rick Riordan",
-  "series_t": "Percy Jackson and the Olympians",
-  "sequence_i": 2,
-  "genre_s": "fantasy",
-  "inStock": true,
-  "price": 6.49,
-  "pages_i": 304
-}, {
-  "id": "978-1857995879",
-  "cat": "book",
-  "name": "Sophie's World : The Greek Philosophers",
-  "author": "Jostein Gaarder",
-  "sequence_i": 1,
-  "genre_s": "fantasy",
-  "inStock": true,
-  "price": 3.07,
-  "pages_i": 64
-}, {
-  "id": "978-1933988177",
-  "cat": "book",
-  "name": "Lucene in Action, Second Edition",
-  "author": "Michael McCandless",
-  "sequence_i": 1,
-  "genre_s": "IT",
-  "inStock": true,
-  "price": 30.50,
-  "pages_i": 475
-}];
-
 EW.define("MainViewport", {
   alias: "mainViewport",
   view: "layout",
@@ -57,8 +13,14 @@ EW.define("MainViewport", {
       {id: "genre_s", header: "Genre", width: 100},
       {id: "cat", header: "Product Group", width: 200}],
     select: "row",
-    defaultData: data[0],
-    data: data
+    defaultData: {
+      name: "select row",
+      author: "select row",
+      genre_s: "select row",
+      cat: "select row"
+    },
+    url: "../data/books.json",
+    dataType: "json"
   }, {
     view: "template",
     itemId: "details",    
@@ -70,7 +32,7 @@ EW.define("MainViewport", {
 });
 
 EW.application({
-  name: "BT",
+  name: "GB",
   requires: [],
   autoViewport: true,
   launch: function (viewport) {
