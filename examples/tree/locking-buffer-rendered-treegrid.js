1c59a97483627da4f90e22913ec54d4b3bb30191
Theo <brainenjii@gmail.com>
2015-05-02 14:49:02 +0300

    locking buffer treegrid added

---
+++ examples/tree/locking-buffer-rendered-treegrid.js
@@ -0,0 +1,25 @@
+EW.define("MainViewport", {
+  alias: "mainViewport",
+  container: "tree-example",
+  rows: [{
+    view: "template", "type": "header", "template": "Forum Folder Summary"
+  }, {
+    view: "treetable",
+    width: 600,
+    height: 400,
+    leftSplit: 1,
+    columns:[{id:"forumtitle", header:"Forum", width:250,
+      template:"{common.treetable()} #forumtitle#"},
+      {id: "username", header: "User", width: 150},
+      {id:"title", header:"title", width:250}
+    ],
+    scroll: true,
+    url: "../data/forum-data.json",
+    dataType: "json"
+  }]
+});
+
+EW.application({
+  name: "Rendered TreeGrid",
+  autoViewport: true
+});
