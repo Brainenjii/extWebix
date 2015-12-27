4b5d037c089d312f41be5364dbf4d67f0c504210
Theo <brainenjii@gmail.com>
2015-12-27 18:51:43 +0300

    script update

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-browserify -x async -x lodash -r extwebix -o ./builds/extwebix.js
+browserify -r extwebix -o ./builds/extwebix.js
 echo "window.EW = require('extwebix');" >> builds/extwebix.js
 
  
